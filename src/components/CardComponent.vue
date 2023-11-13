48d655fb01a909505779d2f8847a6ba0f64f3ff9
ericaostini <ericaostini14@gmail.com>
2023-11-13 16:21:56 +0100

    v-for listCard with file store.js

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -11,7 +11,6 @@
 <script>
 export default {
     name: 'CardComponent',
-    props: ['name', 'type']
 }
 </script>
 
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        <p>ciao</p>
+    <div class="container" v-for="(card, index) in listCard" .:key="index">
+        <CardComponent :image="card.card_image.image_url" :name="card.name" :type='card.archetype' />
     </div>
 </template>
 
 <script>
+import { store } from '../data/store.js';
+import CardComponent from './CardComponent.vue';
 export default {
-    name: 'MainComponent',
+    name: "MainComponent",
+    components: { CardComponent },
+    data() {
+        return {
+            store,
+            listCard: store.listCards
+        }
+    },
 }
 </script>
 
