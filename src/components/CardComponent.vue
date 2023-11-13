1f2d4f9d8b48dc3259fe371958e9924e620b25e8
ericaostini <ericaostini14@gmail.com>
2023-11-13 21:01:08 +0100

    change color text and text-center

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card h-100 bg-mycard">
         <img class="card-img-top" :src="image" :alt="name">
-        <div class="card-body">
-            <h4 class="text-uppercase">{{ name }}</h4>
+        <div class="card-body text-center ">
+            <h4 class="text-uppercase text-light ">{{ name }}</h4>
             <h6>{{ type }}</h6>
         </div>
     </div>
