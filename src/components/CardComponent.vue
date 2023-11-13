e6dcb95f19de406ec1a01ebc7f4f6741c7246b93
ericaostini <ericaostini14@gmail.com>
2023-11-13 15:55:58 +0100

    component card creation with props

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <img :src="image" :alt="name">
+        <div>
+            <h4 class="text-light text-uppercase mb-5">{{ name }}</h4>
+            <h6>{{ type }}</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: ['name', 'type']
+}
+</script>
+
+<style lang="scss" scoped>
+.cards {
+    img {
+        height: 150px;
+        width: 150px;
+    }
+}
+</style>
